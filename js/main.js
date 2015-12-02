5622b1d5d89ea6aae1e5651bdfb8ab8812704b1d
Daniel <danielhuang@DanielHuang.giga.hgc.com.tw>
2015-12-02 16:13:03 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,89 @@
+
+
+$(document).ready(function(){
+	renderCell()
+	render()
+	renderMark()
+	loadApp()
+	calculate()
+	renderCalculate()
+	// intro()
+})
+
+
+function intro () {
+	$('.intro').delay(1000).slideDown();
+}
+
+function renderCell () {
+	for (var i=1; i<10;i++){
+		var cell ='<div><div class="page-container page-'+i+'"></div></div>'
+		$('.flipbook').append(cell);
+	}
+
+	for (var i=1; i<=256;i++){
+		var cell='<span class="num num-'+i+'"></span>';
+		$('.page-container').append(cell);
+	}
+}
+
+function render () {
+	var page = []
+	for (var i=0; i<9;i++){
+		page.push({d:Math.pow(2,i), page:i+1});
+	}
+
+	page.map(function(value){
+		renderNum(value);
+	})
+
+	function renderNum(obj) {
+		for (var i=1, index=0;i*obj.d<500;i=i+2){
+		  for( var j=0;j<obj.d;j++){
+		   var value = i*obj.d+j;
+		   if (value<=500) $('.page-'+obj.page+' .num').eq(index).text(value);
+		   index++;
+		  }  
+		}
+	}
+}
+
+function renderMark () {
+	var cell="",value;
+	$('.page-container').map(function(i,v){
+		// value = Math.pow(2,i);
+		i++;
+		var cell='<div class="mark"><span class="key">'+i+'</span></div>'
+		$(v).after(cell);
+	});
+}
+
+function loadApp() {
+	$('.flipbook').turn({
+			width:1160,
+			height:740,
+			elevation: 50,
+			gradients: true,
+			autoCenter: true,
+			pages:9
+	});
+}
+
+function renderCalculate () {
+	for (var i=1; i<10;i++){
+		var cell = '<li>'+i+'<input type="checkbox" value="'+Math.pow(2,i-1)+'"></li>'
+		$('.calculate ul').append(cell);
+	}
+	$('.calculate ul').append('<li class="submit">送出</li>');
+}
+
+function calculate () {
+	var total=0;
+	$('body').on('click','.submit', function(){
+		$('input:checked').map(function(index,value){
+			console.log(value);
+			total = total + Number($(value).val()); 			
+		})
+		alert(total);
+	})
+}
